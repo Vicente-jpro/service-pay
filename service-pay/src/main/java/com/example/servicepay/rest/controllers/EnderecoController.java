a01c594c4738c8af71f6f388613b4fae03d1dad9
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-07-20 19:52:22 +0100

    End point to get endereco added

--- service-pay/src/main/java/com/example/servicepay/rest/controllers/EnderecoController.java
+++ service-pay/src/main/java/com/example/servicepay/rest/controllers/EnderecoController.java
@@ -2,7 +2,9 @@ package com.example.servicepay.rest.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.servicepay.domain.entities.Endereco;
 import com.example.servicepay.domain.entities.Municipio;
+import com.example.servicepay.domain.repositories.EnderecoRepository;
 import com.example.servicepay.exceptions.EnderecoException;
 import com.example.servicepay.rest.dto.EnderecoRequestDto;
 import com.example.servicepay.rest.dto.EnderecoResponseDto;
@@ -20,7 +23,9 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/enderecos")
@@ -51,24 +56,26 @@ public class EnderecoController {
 		@ApiResponse(code = 201, message = "Contacto salvo com sucesso."),
 		@ApiResponse(code = 404, message = "Erro ao salvar o contacto.")
 	})
-	public Endereco atualizar(@RequestBody EnderecoRequestDto enderecoRequestDTO, @PathVarible("id_endereco") Long idEndereco) {
-		log.info("Atualizando o endereco...");		
-		Endereco enderecoSalvo = getEnderecoById(idEndereco);
-		enderecoRequestDTO.setId(enderecoSalvo.getId());
-		
-		return this.salvar(enderecoRequestDTO);
+	public EnderecoResponseDto atualizar(@RequestBody EnderecoRequestDto enderecoRequestDTO, @PathVariable("id_endereco") Long idEndereco) {	
+		Endereco enderecoSalvo = this.enderecoService.atualizar(enderecoRequestDTO, idEndereco);
+		EnderecoResponseDto enderecoResponseDto = modelMapper.map(enderecoSalvo, EnderecoResponseDto.class);
+		return enderecoResponseDto;
 	}
 	
-	public Endereco getEnderecoById(Long idEndereco) {
-		log.info("Buscando o endereco co ID: {}", idEndereco);
+	@GetMapping("/{id_endereco}")
+	@ApiOperation("Buscar endereco pelo ID.")
+	@ResponseStatus(HttpStatus.OK)
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "Endereco encontrado com sucesso."), 
+		@ApiResponse(code = 404, message = "Parametro invalido")
+	})
+	public EnderecoResponseDto getEnderecoById(Long idEndereco) {
 		
-		Endereco endereco = enderecoRepository.findById(idEndereco).get();
-		if(endereco == null) {
-			log.error("Endereco nao encontrado ID: {}", idEndereco);
-			throw new EnderecoException("Endereco nao encontrado.");
-		}
-		
-		return endereco;
+		Endereco endereco = this.enderecoService.getEnderecoById(idEndereco);
+		EnderecoResponseDto enderecoResponseDto = this.modelMapper.map(endereco, EnderecoResponseDto.class);
+		return enderecoResponseDto;
 	}
+	
+	
 	
 }
